3b1cc28b159ea383453757107253bca4bdda0cd6
Arno Princeston Pan <apan11@my.bcit.ca>
2023-07-04 15:45:09 -0700

    listed out tasks

---
+++ components/Task.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Task = (props) => {
+  return(
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text styles={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  item : {
+    backgroundColor: "#FFF",
+    padding: 14,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 18,
+  },
+  itemLeft : {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: 'lightblue',
+    opacity: 0.4,
+    borderRadius: 8,
+    marginRight: 16,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 16,
+    height: 16,
+    borderColor: '#55bcf6',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Task;
